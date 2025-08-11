440d174ea81fe95b029633f4364c43de6a4f9b38
naTan-Sa <natansahlu15@gmail.com>
2025-08-11 22:29:26 +0000

    Add C program that prints a quote to standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - A C program that prints a message
+ *
+ * Return: 1 (Success)
+*/
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, ", stdout);
+	fputs("2015-10-19\n", stdout);
+	return (1);
+}
