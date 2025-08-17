959a9060d507894d453042ac2c103476fdc35924
naTan-Sa <natansahlu15@gmail.com>
2025-08-17 22:35:28 +0000

    2-print_alphabet.c: print lowercase alphabet using putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return : 0 if success
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
