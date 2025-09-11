9444c85c98caf098c27915917e4d2b766fa31eda
naTan-Sa <natansahlu15@gmail.com>
2025-09-11 22:02:41 +0000

    Add solution for task 11: largest prime factor of 612852475143

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - finds and prints the largest prime factor of 612852475143
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	long n = 612852475143;
+	long largest = -1;
+	int i;
+
+	while (n % 2 == 0)
+	{
+		largest = 2;
+		n /= 2;
+	}
+
+	for (i = 3; i <= sqrt(n); i += 2)
+	{
+		while (n % i == 0)
+		{
+			largest = i;
+			n /= i;
+		}
+	}
+
+	if (n > 2)
+		largest = n;
+
+	printf("%ld\n", largest);
+	return (0);
+}
