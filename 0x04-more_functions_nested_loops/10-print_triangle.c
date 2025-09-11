05b724da1ac4f55ca3f2fb8744bd5cbcbc449dec
naTan-Sa <natansahlu15@gmail.com>
2025-09-11 21:44:11 +0000

    Add function to print a triangle using '#' with proper alignment

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -12,20 +12,16 @@ void print_triangle(int size)
 	int j;
 
 	if (size <= 0)
-	{
 		_putchar('\n');
-	}
 
 	for (i = 1; i <= size; i++)
 	{
 		for (j = 0; j < size - i; j++)
-		{
 			_putchar(' ');
-		}
+
 		for (j = 0; j < i; j++)
-		{
 			_putchar('#');
-		}
+
 		_putchar('\n');
 	}
 }
