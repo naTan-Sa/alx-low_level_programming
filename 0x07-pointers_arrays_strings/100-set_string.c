c5cab3cb23aeb222ef00f7cabbc940ce90d36373
naTan-Sa <natansahlu15@gmail.com>
2025-10-12 09:43:15 +0000

    Implement set_string to make a pointer point to another string using double pointer

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: double pointer to be modified
+ * @to: string to point to
+ *
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
