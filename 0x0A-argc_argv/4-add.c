71c2eb288f12bb9b9be1a09ddaffe75cd282a0da
naTan-Sa <natansahlu15@gmail.com>
2025-10-17 06:50:07 +0000

    0x0A-argc_argv: add positive numbers with validation and error handling

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - adds positive numbers
+ * @argc: number of command-line arguments
+ * @argv: array of strings containing the arguments
+ *
+ * Return: 0 if success, 1 if invalid number found
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, sum = 0;
+	
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1;i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
