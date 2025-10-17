6a867a897a71ca8868fe09cea2ea96c448b0bed3
naTan-Sa <natansahlu15@gmail.com>
2025-10-17 19:28:13 +0000

    edited: betty style

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,14 +11,14 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, j,sum = 0;
+	int i, j, sum = 0;
 
 	if (argc == 1)
 	{
 		printf("0\n");
 		return (0);
 	}
-	for (i = 1;i < argc; i++)
+	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
