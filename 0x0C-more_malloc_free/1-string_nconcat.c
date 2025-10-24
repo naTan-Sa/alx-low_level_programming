b22a8d69fbe3ab7db477b3489f808eddf411df27
naTan-Sa <natansahlu15@gmail.com>
2025-10-24 10:28:22 +0000

    Add string_nconcat function to concatenate two strings with n bytes

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdlib>
+
+/**
+ * string_nconcat - 
+ * @s1: first string
+ * @s2: seconf string
+ * @n: number of bytes from s2 to cooncatenate
+ *
+ * Return: pointer to newly allocated string,
+ * 	or NULL if memor allocation fails
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int len1 = 0, len2 = 0, i, j;
+
+	char *str;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[len1])
+		len1++;
+	while (s2[len2])
+		len2++;
+
+	if (n >= len2)
+		n = len2;
+
+
+	str = malloc(sizeof(char) * (len1 + n + 1));
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; i < len1; i++)
+		str[i] = s1[i];
+	for (j = 0; j < n; j++)
+		str[i + j] = s2[j];
+
+	str[i + j] = '\0';
+
+	return (str);
+}
