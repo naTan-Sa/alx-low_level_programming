2129f3d2f140072733e3f1320e96d7775ea9aa9a
naTan-Sa <natansahlu15@gmail.com>
2025-10-31 13:59:11 +0000

    Define struct dog with name, age, and owner fields

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Defines a dogs basic info
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* DOG_H */
