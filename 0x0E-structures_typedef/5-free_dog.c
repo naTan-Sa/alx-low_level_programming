156052a0911e533e5523b09d3c490c9cc6175e84
naTan-Sa <natansahlu15@gmail.com>
2025-11-01 05:02:03 +0000

    Implement free_dog function to safely deallocate memory for struct dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees dogs
+ * @d: pointer to dog_t to free
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	if (d->name)
+		free(d->name);
+	if (d->owner)
+		free(d->owner);
+	free(d);
+}
