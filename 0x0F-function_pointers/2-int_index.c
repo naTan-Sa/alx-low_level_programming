14cb355379c8448b6d0560ec09b8a61a6c75ab85
naTan-Sa <natansahlu15@gmail.com>
2025-11-03 05:58:42 +0000

    0x0F. C - Function pointers: implement int_index to search an integer in an array

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,5 +1,14 @@
 #include "function_pointers.h"
 
+/**
+ * int_index - searches for an integer in an array using a comparison function
+ * @array: array of integers
+ * @size: number of elements in the array
+ * @cmp: pointer to the function used to compare values
+ *
+ * Return: index of the first element for which cmp does not return 0
+ *         -1 if no element matches or size <= 0
+ */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
@@ -14,4 +23,3 @@ int int_index(int *array, int size, int (*cmp)(int))
 	}
 	return (-1);
 }
-
