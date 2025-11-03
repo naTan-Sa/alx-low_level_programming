7ad8d67b246392e5e334157818e01240b7158bc6
naTan-Sa <natansahlu15@gmail.com>
2025-11-03 06:24:55 +0000

    task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,11 +5,13 @@
  * print_name - prints a name using a given function
  * @name: name of the person
  * @f: pointer to a function that takes a string
+ *
+ * Return: nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
 			return;
-
-	*(f)(name);
+	
+	f(name);
 }
