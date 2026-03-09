894d77c8697f169f44c597f39532f64cf17a114d
naTan-Sa <natansahlu15@gmail.com>
2026-03-09 09:29:40 +0000

    Implement function to delete head node and return its data

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,6 +1,11 @@
 #include "lists.h"
 
-
+/**
+ * pop_listint - deletes the head node of a listint_t list
+ * @head: pointer to pointer of the head node
+ *
+ * Return: the data (n) of the deleted node, or 0 if the list is empty
+ */
 int pop_listint(listint_t **head)
 {
 	int value;
