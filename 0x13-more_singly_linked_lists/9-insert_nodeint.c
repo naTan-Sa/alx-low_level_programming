4410338519266c1e5953be35a75b4411a5f661f5
naTan-Sa <natansahlu15@gmail.com>
2026-03-11 08:10:38 +0000

    Implement function to insert node at a given index in linked list

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+
+
+/**
+ * insert_nodeint_at_index - inserts a new node at a given position
+ * @head: pointer to the head of the list
+ * @idx: index where the new node should be added
+ * @n: value to insert in the new node
+ *
+ * Return: address of the new node, or NULL if it fails
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new_node;
+	listint_t *temp;
+	unsigned int count = 0;
+
+	 if (head == NULL)
+                return (NULL);
+
+	temp = *head;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+
+	while (temp != NULL)
+	{
+		if (count == idx)
+		{
+			new_node->next = temp->next;
+			temp->next = new_node;
+			return (new_node);
+		}
+
+		temp = temp->next;
+		count++;
+	}
+
+	free (new_node);
+	return (NULL);
+}
